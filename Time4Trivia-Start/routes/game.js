c4d16bcf6d1f8adc9b475ad9bc32cf9217abe065
jespinoza12 <jespinoza@student.neumont.edu>
2023-07-12 10:44:12 -0600

    Requires Login to Play

--- Time4Trivia-Start/routes/game.js
+++ Time4Trivia-Start/routes/game.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/play', function(req, res, next) {
   // TODO: Implement Game
-  
-  res.render('play', {user: req.session.user});
+  if (!req.session.user) {
+    res.redirect('/u/login');
+  }else if (req.session.user) {
+    res.render('play', {user: req.session.user});
+  }
 });
 
 module.exports = router;
